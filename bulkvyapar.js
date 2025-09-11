cc0f50c3487d0c36273df5c275b69f17d314b327
Mohammad Javed <javedmohammad485@gmail.com>
2025-09-11 23:59:21 +0530

    Uploaded

---
+++ bulkvyapar.js
@@ -0,0 +1,161 @@
+let project = [
+    {
+        id: 1,
+        projectImage: "assets/projects/bulk/bulkvyapar.png",
+        projectName: "Bulkvyapar Network",
+        projectDetail: "I designed and developed the BulkVyapar website from scratch using HTML, CSS, Bootstrap, and JavaScript. My role was focused on UI/UX design and frontend development, ensuring the platform was clean, responsive, and user-friendly for B2B buyers and suppliers.",
+        tech: ['HTML', 'CSS', 'BOOTSTRAP', 'JAVASCRIPT', 'PHP', 'LARAVEL' ],
+        link: "https://bulkvyapar.com/",
+    },
+    {
+        id: 2,
+        projectImage: "assets/projects/bulk/find.png",
+        projectName: "Find Distributer",
+        projectDetail: "To find distributors for your product, first, prepare a comprehensive information package detailing your business, target market, pricing, and support plans. Then, leverage industry-specific online marketplaces and attend trade shows to find and contact potential distributors directly. Lastly, evaluate their motivation and capability, and be ready to offer marketing support to build a strong partnership and ensure successful distribution.",
+        tech: ['HTML', 'CSS', 'BOOTSTRAP', 'JAVASCRIPT' ],
+        link: "https://javed485.github.io/find-distributer/",
+    },
+    {
+        id: 3,
+        projectImage: "assets/projects/bulk/culture.png",
+        projectName: "Culture Wedding",
+        projectDetail: "Culture Wedding is a beautifully designed website that showcases traditional wedding services, themes, and cultural values. The goal was to create an elegant, user-friendly platform where couples and families can explore wedding ideas, book services, and learn about cultural traditions.",
+        tech: ['HTML', 'CSS', 'BOOTSTRAP', 'JAVASCRIPT' ],
+        link: "",
+    },
+    {
+        id: 4,
+        projectImage: "assets/projects/bulk/gum king.jpg",
+        projectName: "Gumking",
+        projectDetail: "At Gumking, we pride ourselves on being a premier manufacturer and exporter of high-quality guar gum products and their derivatives. With years of industry experience and a commitment to excellence, we serve clients worldwide with a wide range of applications for guar gum.",
+        tech: ['HTML', 'CSS', 'BOOTSTRAP', 'JAVASCRIPT' ],
+        link: "https://gumking.in/",
+    },
+    {
+        id: 5,
+        projectImage: "assets/projects/bulk/mag pow.jpg",
+        projectName: "Magnet Powertech",
+        projectDetail: "Magnet Powertech Is Leading Supplier and Manufacturer of Solar Energy Products and Domestic and Commercial LED Lighting Products. Bright Power System Technology is in Existence since year 2017.",
+        tech: ['HTML', 'CSS', 'BOOTSTRAP', 'JAVASCRIPT' ],
+        link: "https://magnetpowertech.com/",
+    },
+    {
+        id: 6,
+        projectImage: "assets/projects/bulk/chemical.png",
+        projectName: "Prosperous World Chemical",
+        projectDetail: "Prosperous world chemical is one of leading firm in laboratory outsourcing services, in Jharkhand State, India. We provide technical know-how, trained staff and specialized chemists to perform various regular test of your chemistry lab, in various running plants, like Steel, Cement, Water treatment etc.",
+        tech: ['HTML', 'CSS', 'BOOTSTRAP', 'JAVASCRIPT' ],
+        link: "https://www.pwchemical.in/",
+    },
+    {
+        id: 7,
+        projectImage: "assets/projects/bulk/fab four.jpg",
+        projectName: "Fab Four Technoplast",
+        projectDetail: "Fab Four Technoplast , an ISO 9001:2015 certified company, is one of the leading manufacturers and exporters of Plastic Injection Molded parts in India . In 2022, Fab Four Industries, a leading plastic injection moulding company was established by a group of Four Fabulous friends based in Rudrapur and Kashipur.",
+        tech: ['HTML', 'CSS', 'BOOTSTRAP', 'JAVASCRIPT' ],
+        link: "https://fabfour.in/",
+    },
+    {
+        id: 8,
+        projectImage: "assets/projects/bulk/sab electric.jpg",
+        projectName: "Sab Electrical",
+        projectDetail: "",
+        tech: ['HTML', 'CSS', 'BOOTSTRAP', 'JAVASCRIPT' ],
+        link: "",
+    },
+    {
+        id: 9,
+        projectImage: "assets/projects/bulk/G-k-Industry.png",
+        projectName: "GK Industry",
+        projectDetail: "To create a clean, intuitive, and conversion-optimized interface that highlights the company's leadership in the disposable machinery industry, showcases products effectively, and enhances the user's trust and buying decision.",
+        tech: ['HTML', 'CSS', 'BOOTSTRAP', 'JAVASCRIPT' ],
+        link: "",
+    },
+    {
+        id: 10,
+        projectImage: "assets/projects/bulk/A-V-car.png",
+        projectName: "AV car Accessories",
+        projectDetail: "The User Interface (UI) of the AV Car Accessories website is crucial in delivering a sleek, modern, and user-centric shopping experience for car enthusiasts and everyday drivers alike. The UI should present a visually appealing layout that makes it easy for users to browse various car accessories, from tech gadgets to performance upgrades.",
+        tech: ['HTML', 'CSS', 'BOOTSTRAP', 'JAVASCRIPT' ],
+        link: "https://av-car-accessories.netlify.app/",
+    },
+    {
+        id: 11,
+        projectImage: "assets/projects/bulk/Ganpati-industries.png",
+        projectName: "Ganpati Industries",
+        projectDetail: "The interface should guide users seamlessly through product offerings, certifications, and applications, while emphasizing the company’s experience and quality assurance. Strategic placement of visuals, product data, and inquiry forms will ensure a user-friendly experience that drives engagement, builds trust, and converts visitors into clients.",
+        tech: ['HTML', 'CSS', 'BOOTSTRAP', 'JAVASCRIPT' ],
+        link: "",
+    },
+    {
+        id: 12,
+        projectImage: "assets/projects/bulk/Maa-Mansha.png",
+        projectName: "Maa Mansha Cycle",
+        projectDetail: "The User Interface (UI) of Maa Mansha Cycle Company’s website plays a vital role in presenting a seamless and informative experience for users looking to explore, evaluate, or purchase bicycles. It should be designed to clearly display different cycle models, specifications, and pricing, along with tools or guides to help users determine the value of their cycles based on make, model, and condition.",
+        tech: ['HTML', 'CSS', 'BOOTSTRAP', 'JAVASCRIPT' ],
+        link: "",
+    }
+];
+
+let projectContainer = document.querySelector('.project-container');
+
+function projectsBox(){
+    project.forEach((el) => {    
+        let projectBox = document.createElement('div');
+        projectBox.classList.add('project-box', 'project-detail');
+        projectBox.setAttribute('data-aos', 'fade-up');
+
+        let imgBox = document.createElement('div');
+        imgBox.classList.add('project-img');
+        
+        let pImage = document.createElement('img');
+        pImage.src = el.projectImage;
+        pImage.alt = el.projectName;
+    
+        imgBox.appendChild(pImage);
+    
+        let projectText = document.createElement('div');
+        projectText.classList.add('project-text', 'projectsBtn');
+
+        let h3 = document.createElement('h3');
+        h3.innerText = el.projectName;
+
+        let p = document.createElement('p');
+        p.textContent = el.projectDetail;
+
+        let ul = document.createElement('ul');
+        el.tech.forEach((skill) => {
+            let li = document.createElement('li');
+            li.textContent = skill;
+            ul.appendChild(li);
+        });
+
+        let liLink = document.createElement('li');
+        let link = document.createElement('a');
+        link.href = el.link;
+        link.target = '_blank';
+        link.innerText = 'Live Link';
+        
+        liLink.appendChild(link);
+        ul.appendChild(liLink);
+        projectText.append(h3, p, ul);
+        projectBox.append(imgBox, projectText);
+    
+        projectContainer.append(projectBox);
+    });
+};
+
+projectsBox();
+
+
+
+
+
+
+
+
+
+
+
+
+
